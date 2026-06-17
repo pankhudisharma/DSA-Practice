e79aad38003b6b16332e36a4f7a5185702e028c9
Pankhudi Sharma <pankhudis029@gmail.com>
2026-06-17 17:45:01 +0530

    Alternate elements

---
+++ Arrays/002_AlternateElements.py
@@ -0,0 +1,12 @@
+def getAlternates(arr):
+    res = []
+    
+    # Iterate over all alternate elements
+    for i in range(0, len(arr), 2):
+        res.append(arr[i])
+    return res
+
+if __name__ == "__main__":
+    arr = [10, 20, 30, 40, 50]
+    res = getAlternates(arr)
+    print(" ".join(map(str, res)))
