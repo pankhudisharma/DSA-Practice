032d8e7930aa289e78a0d989861bfedad886e510
Pankhudi Sharma <pankhudis029@gmail.com>
2026-07-19 15:39:44 +0530

    Check if Two Strings are Same

---
+++ Strings/002_SameStrings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def areStringsSame(self, s1, s2):
+
+        return s1 == s2
+
+
+s1 = "Hello"
+s2 = "Hello"
+
+obj = Solution()
+
+if obj.areStringsSame(s1,s2):
+    print("yes")
+else:
+    print("no")    
